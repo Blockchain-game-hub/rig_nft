1c4eeb58df1a822b0677615a640bd6c219937439
J_TDZ <93098700+mooshmalon3@users.noreply.github.com>
2021-11-10 13:06:27 -0800

    Delete cpu directory

--- cpu/cpu_metadata.py
+++
@@ -1,6 +0,0 @@
-metadata_template = {
-    "description": "",
-    "name": "",
-    "external_url": "",
-    "attributes": [{"trait_type": "Clock Speed", "value": ""}, {"trait_type": "Core Count", "value": ""}, {"trait_type": "Wattage", "value": ""}, {"trait_type": "Grade", "value": ""}],
-}
