c07a1c9b4c67067ec9ec42b0db1c3e0e5a762b49
J_TDZ <93098700+mooshmalon3@users.noreply.github.com>
2021-11-10 13:06:42 -0800

    Delete case directory

--- case/case_metadata.py
+++
@@ -1,6 +0,0 @@
-metadata_template = {
-    "description": "",
-    "name": "",
-    "external_url": "",
-    "attributes": [{"trait_type": "form_factor", "value": ""}, {"trait_type": "air_flow", "value": ""}, {"trait_type": "Grade", "value": ""}, {"trait_type": "Grade", "value": ""}],
-}
